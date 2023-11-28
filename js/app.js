4416808a1ac604c5137e1713c75af3670ecda40d
duridev <duridev@outlook.com>
2023-11-27 23:49:56 -0300

    Generar select de años

--- js/app.js
+++ js/app.js
@@ -2,13 +2,20 @@
 
 const resultado = document.querySelector('#resultado');
 
+const year = document.querySelector('#year')
+const max = new Date().getFullYear();
+const min = max - 13;
+
 
 
 // *************     EVENTOS     ***************************
 
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();
-})
+    mostrarAutos(); // Muestra los autos al cargar
+
+    // Llena las opciones de años
+    llenarSelect();
+});
 
 
 // *************     FUNCIONES    ***************************
@@ -24,4 +31,17 @@ function mostrarAutos() {
         // insertar en el html
         resultado.appendChild(autoHTML)
     });
+}
+
+//-----------
+
+
+//Genera los años del select
+function llenarSelect(){
+    for(let i = max; i >= min; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);// agrega las opciones de año al select
+    }
 }
