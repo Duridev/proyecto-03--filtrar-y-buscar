02be5d6772f0c98efa8129d64954fe273cd70aa2
duridev <duridev@outlook.com>
2023-11-28 00:03:23 -0300

    leer valores de automoviles

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 // *************     VARIABLES     ***************************
+const marca = document.querySelector('#marca')
+const year = document.querySelector('#year')
+const minimo = document.querySelector('#minimo')
+const maximo = document.querySelector('#year')
+const puertas = document.querySelector('#puertas')
+const transmision = document.querySelector('#transmision')
+const color = document.querySelector('#color')
 
+
+//Contenedoir para los resultados
 const resultado = document.querySelector('#resultado');
 
-const year = document.querySelector('#year')
 const max = new Date().getFullYear();
 const min = max - 13;
+
+
+
+//Generar un objeto con la busqyeda
+const datosBusqueda = {
+    marca : '',
+    year : '',
+    minimo : '',
+    maximo : '',
+    puertas : '',
+    transmision : '',
+    color : ''
+};
 
 
 
@@ -16,6 +37,39 @@ document.addEventListener('DOMContentLoaded', () => {
     // Llena las opciones de años
     llenarSelect();
 });
+
+// ----------------
+
+//Event listener para los select de búsqueda
+marca.addEventListener('change', (e) =>{
+    datosBusqueda.marca = e.target.value;
+});
+ 
+year.addEventListener('change', (e) =>{
+    datosBusqueda.year = e.target.value;
+});
+ 
+minimo.addEventListener('change', (e) =>{
+    datosBusqueda.minimo = e.target.value;
+});
+ 
+maximo.addEventListener('change', (e) =>{
+    datosBusqueda.maximo = e.target.value;
+});
+ 
+puertas.addEventListener('change', (e) =>{
+    datosBusqueda.puertas = e.target.value;
+});
+ 
+transmision.addEventListener('change', (e) =>{
+    datosBusqueda.transmision = e.target.value;
+});
+ 
+color.addEventListener('change', (e) =>{
+    datosBusqueda.color = e.target.value;
+});
+ 
+
 
 
 // *************     FUNCIONES    ***************************
@@ -35,7 +89,6 @@ function mostrarAutos() {
 
 //-----------
 
-
 //Genera los años del select
 function llenarSelect(){
     for(let i = max; i >= min; i--) {
@@ -45,3 +98,6 @@ function llenarSelect(){
         year.appendChild(opcion);// agrega las opciones de año al select
     }
 }
+
+//-----------
+
